28c8aa59e056e312843bbac8f769022d2911f878
taisleal <tais.almeida.dev@gmail.com>
2025-01-31 19:54:35 -0300

    update

--- logica-js-projeto_inicial-desafio-dois/app.js
+++ logica-js-projeto_inicial-desafio-dois/app.js
@@ -56,3 +56,84 @@ function numberSquad(number){
 }
 
 numberSquad(5)
+
+
+function calculeBMI(weight,height){
+
+    height = height * height;
+
+    let BMI = weight / height;
+
+    return BMI; 
+
+}
+
+let BMI = calculeBMI(58,1.60)
+console.log(BMI)
+
+
+function fatorNumber(number){
+    let aux = number; 
+    let result =1;
+
+    while(aux>=1 ){
+
+        result *= aux 
+       aux--; 
+    }
+    return result
+}
+
+let result = fatorNumber(5);
+console.log(result)
+
+
+function dollar(moneyReal) {
+
+    return moneyReal / 5.87;
+    
+}
+
+let resultDollar = dollar(0.5)
+console.log(resultDollar)
+
+
+function perimeterRectangle(height, width){
+   return height * width;
+}
+
+result = perimeterRectangle(10,10)
+console.log(result)
+
+function calculeAreaPerimeter(radius){
+    let area = Math.pow(radius,2) * 3.14;
+
+    let circumference = 2 * 3.14 * radius;
+
+    return {area, circumference}; 
+}
+
+result = calculeAreaPerimeter(10)
+console.log(`area: ${result.area} cm²`);
+console.log(`Perímeter: ${result.circumference} cm`);
+//console.log(result)
+
+function createMultiplicationTable(number){
+
+    let aux = 0;
+    let table;
+    let show;
+    
+    while(aux <= 10){
+        table = aux * number;
+        show = console.log(table)
+        aux++; 
+        
+        
+    }
+    return show;
+}
+
+result = createMultiplicationTable(5)
+console.log(result)
+
